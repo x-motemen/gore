2d6085974aa2358ec85f5492382c34698efc2078
motemen <motemen@gmail.com>
2015-02-17 09:25:58 +0900

    split source

---
+++ complete.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"strings"
+)
+
+func (s *Session) completeWord(line string, pos int) (string, []string, string) {
+	if strings.HasPrefix(line, ":") {
+		// complete commands
+		if !strings.Contains(line[0:pos], " ") {
+			pre, post := line[0:pos], line[pos:]
+
+			result := []string{}
+			for _, command := range commands {
+				name := ":" + command.name
+				if strings.HasPrefix(name, pre) {
+					// having complete means that this command takes an argument (for now)
+					if !strings.HasPrefix(post, " ") && command.arg != "" {
+						name = name + " "
+					}
+					result = append(result, name)
+				}
+			}
+			return "", result, post
+		}
+
+		// complete command arguments
+		for _, command := range commands {
+			if command.complete == nil {
+				continue
+			}
+
+			cmdPrefix := ":" + command.name + " "
+			if strings.HasPrefix(line, cmdPrefix) && pos >= len(cmdPrefix) {
+				return cmdPrefix, command.complete(line[len(cmdPrefix):pos]), ""
+			}
+		}
+
+		return "", nil, ""
+	}
+
+	if gocode.invalid {
+		return "", nil, ""
+	}
+
+	// code completion
+
+	s.clearQuickFix()
+
+	source, err := s.source(false)
+	if err != nil {
+		errorf("source: %s", err)
+		return "", nil, ""
+	}
+
+	p := strings.LastIndex(source, "}")
+	editSource := source[0:p] + line + source[p:]
+	cursor := len(source[0:p]) + pos
+
+	cands, err := gocode.complete(editSource, cursor)
+	if err != nil {
+		errorf("gocode: %s", err)
+		return "", nil, ""
+	}
+
+	return line[0:pos], cands, ""
+
+}
--- main.go
+++ main.go
@@ -53,67 +53,7 @@ func main() {
 		}
 	}
 
-	rl.SetWordCompleter(func(line string, pos int) (string, []string, string) {
-		if strings.HasPrefix(line, ":") {
-			// complete commands
-			if !strings.Contains(line[0:pos], " ") {
-				pre, post := line[0:pos], line[pos:]
-
-				result := []string{}
-				for _, command := range commands {
-					name := ":" + command.name
-					if strings.HasPrefix(name, pre) {
-						// having complete means that this command takes an argument (for now)
-						if !strings.HasPrefix(post, " ") && command.arg != "" {
-							name = name + " "
-						}
-						result = append(result, name)
-					}
-				}
-				return "", result, post
-			}
-
-			// complete command arguments
-			for _, command := range commands {
-				if command.complete == nil {
-					continue
-				}
-
-				cmdPrefix := ":" + command.name + " "
-				if strings.HasPrefix(line, cmdPrefix) && pos >= len(cmdPrefix) {
-					return cmdPrefix, command.complete(line[len(cmdPrefix):pos]), ""
-				}
-			}
-
-			return "", nil, ""
-		}
-
-		if gocode.invalid {
-			return "", nil, ""
-		}
-
-		// code completion
-
-		s.clearQuickFix()
-
-		source, err := s.source(false)
-		if err != nil {
-			errorf("source: %s", err)
-			return "", nil, ""
-		}
-
-		p := strings.LastIndex(source, "}")
-		editSource := source[0:p] + line + source[p:]
-		cursor := len(source[0:p]) + pos
-
-		cands, err := gocode.complete(editSource, cursor)
-		if err != nil {
-			errorf("gocode: %s", err)
-			return "", nil, ""
-		}
-
-		return line[0:pos], cands, ""
-	})
+	rl.SetWordCompleter(s.completeWord)
 
 	for {
 		in, err := rl.Prompt()
