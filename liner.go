d911530da69a47826d4b0a2e5f93cbdeb2eddb37
Takashi Kokubun <takashikkbn@gmail.com>
2015-02-20 00:27:17 +0900

    Indent code in continue mode

--- liner.go
+++ liner.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"io"
+	"strings"
+	"text/scanner"
 
 	"github.com/peterh/liner"
 )
@@ -10,11 +13,13 @@ import (
 const (
 	promptDefault  = "gore> "
 	promptContinue = "..... "
+	indent         = "    "
 )
 
 type contLiner struct {
 	*liner.State
 	buffer string
+	depth  int
 }
 
 func newContLiner() *contLiner {
@@ -24,7 +29,11 @@ func newContLiner() *contLiner {
 
 func (cl *contLiner) promptString() string {
 	if cl.buffer != "" {
-		return promptContinue
+		prompt := promptContinue
+		for i := 0; i < cl.depth; i++ {
+			prompt += indent
+		}
+		return prompt
 	}
 
 	return promptDefault
@@ -53,4 +62,55 @@ func (cl *contLiner) Prompt() (string, error) {
 func (cl *contLiner) Accepted() {
 	cl.State.AppendHistory(cl.buffer)
 	cl.buffer = ""
+}
+
+func (cl *contLiner) Reindent() {
+	var min, max int
+	newDepth := cl.countDepth()
+
+	if newDepth > cl.depth {
+		min, max = cl.depth, newDepth
+	} else {
+		min, max = newDepth, cl.depth
+	}
+
+	lines := strings.Split(cl.buffer, "\n")
+	lastLine := lines[len(lines)-1]
+
+	prompt := promptDefault
+	if len(lines) > 1 {
+		prompt = promptContinue
+	}
+	fmt.Printf("\x1b[1A\r%s", prompt)
+
+	cl.printIndent(min)
+	fmt.Print(lastLine)
+	cl.printIndent(max - min)
+	fmt.Print("\n")
+
+	cl.depth = newDepth
+}
+
+func (cl *contLiner) countDepth() int {
+	reader := bytes.NewBufferString(cl.buffer)
+	sc := new(scanner.Scanner)
+	sc.Init(reader)
+
+	depth := 0
+	for {
+		switch sc.Scan() {
+		case '{':
+			depth++
+		case '}':
+			depth--
+		case scanner.EOF:
+			return depth
+		}
+	}
+}
+
+func (cl *contLiner) printIndent(count int) {
+	for i := 0; i < count; i++ {
+		fmt.Print(indent)
+	}
 }
--- main.go
+++ main.go
@@ -88,6 +88,7 @@ func main() {
 			continue
 		}
 
+		rl.Reindent()
 		err = s.Eval(in)
 		if err != nil {
 			if err == ErrContinue {
