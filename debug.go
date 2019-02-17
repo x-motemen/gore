a1ba02a978f42ebbe1167a1244300875e7a7d52c
itchyny <itchyny@hatena.ne.jp>
2019-02-17 22:12:16 +0900

    refactor debugf using build tags and reduce runtime branching

--- debug.go
+++ debug.go
@@ -2,6 +2,19 @@
 
 package main
 
-func init() {
-	debug = true
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"runtime"
+)
+
+func debugf(format string, args ...interface{}) {
+	_, file, line, ok := runtime.Caller(1)
+
+	if ok {
+		format = fmt.Sprintf("%s:%d %s", filepath.Base(file), line, format)
+	}
+
+	fmt.Fprintf(os.Stderr, format+"\n", args...)
 }
