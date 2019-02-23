92d72039fffa753359b732c0a892b1bebef720ab
itchyny <itchyny@hatena.ne.jp>
2019-02-24 00:41:58 +0900

    remove comments in main.go

--- main.go
+++ main.go
@@ -1,20 +1,3 @@
-/*
-Yet another Go REPL that works nicely. Featured with line editing, code completion and more.
-
-Usage
-
-When started, a prompt is shown waiting for input. Enter any statement or expression to proceed.
-If an expression is given or any variables are assigned or defined, their data will be pretty-printed.
-
-Some special functionalities are provided as commands, which starts with colons:
-
-	:import <package path>  Imports a package
-	:print                  Prints current source code
-	:write [<filename>]     Writes out current code
-	:doc <target>           Shows documentation for an expression or package name given
-	:help                   Lists commands
-	:quit                   Quit the session
-*/
 package main
 
 import (
