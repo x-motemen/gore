2d94e7255eaf820cbc6a0866f79e33453d86e8d4
itchyny <itchyny@hatena.ne.jp>
2021-01-09 22:30:19 +0900

    fix a typo in comments

--- session.go
+++ session.go
@@ -556,7 +556,7 @@ func (s *Session) importFile(src []byte) error {
 			if isNamedIdent(funcDecl.Name, "main") {
 				f.Decls = append(f.Decls[0:i], f.Decls[i+1:]...)
 				// main() removed from this file, we may have to
-				// remove some unsed import's
+				// remove some unused import's
 				quickfix.QuickFix(s.fset, []*ast.File{f})
 				break
 			}
